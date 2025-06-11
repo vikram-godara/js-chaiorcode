24ecf8d826e44c6b9b564c668bd7f40477240193
vikram-godara <vikramgodaraofficial59@gmail.com>
2025-06-11 12:33:10 +0000

    ✅ Commit without GPG signing

---
+++ 01_basic/02_dataTypes..js
@@ -0,0 +1,28 @@
+"use strict"; //treat as all of new version
+
+// alert(3+3); //we use node.js ,not browser
+
+console.log(3 + 3);
+let state;
+
+
+let name = "hitesh";
+let age  = 18;
+let isloggedIn = false;
+
+//number =>2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null =>standalone value //abhi valuse asgin nhi hai
+
+//symbol => unique value    
+
+
+
+//object =>
+console.log(typeof(undefined));
+console.log(typeof(null));
+
+
+    
